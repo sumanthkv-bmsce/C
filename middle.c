4461be49e3f12e27443c338853edb4efed20cd5d
sumanthkv-bmsce <44916968+sumanthkv-bmsce@users.noreply.github.com>
2019-09-13 01:13:12 +0530

    Add files via upload

---
+++ middle.c
@@ -0,0 +1,23 @@
+/* Link list node */
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+
+/* Function to get the middle of the linked list*/
+void printMiddle(struct Node *head)
+{
+    struct Node *slow_ptr = head;
+    struct Node *fast_ptr = head;
+
+    if (head!=NULL)
+    {
+        while (fast_ptr != NULL && fast_ptr->next != NULL)
+        {
+            fast_ptr = fast_ptr->next->next;
+            slow_ptr = slow_ptr->next;
+        }
+        printf("The middle element is [%d]\n\n", slow_ptr->data);
+    }
+}
